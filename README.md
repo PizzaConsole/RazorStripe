5e37b82ebf9d5805025359f3d5f7ac645d674aa4
PoisnFang <poisnfang@poisnfang.com>
2020-03-05 13:18:11 -0700

    Emails and Stripe tested and working

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ RazorStripe is an ASP.NET Core 3.1 Template that combines Razor Pages with Strip
 ## Run the Project
 1. Download/fork the project and run the .sln
 2. Add your own stripe keys in the [appsettings](appsettings.json)
-3. Add your credentials in the [EmailSender](/Services/EmailSender.cs)
+3. Add your credentials in the [EmailSender](/Services/EmailSender.cs) - Emails are sent using SMTP
 4. Re-save the [libman file](libman.json) and verify that the 5 folders have been downloaded into the [wwwroot](/wwwroot)
 5. add-migration Initial
 6. update-database
--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -1,41 +1,35 @@
 using Microsoft.AspNetCore.Identity.UI.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace RazorStripe.Services
 {
-    // This class is used by the application to send email for account confirmation and password reset.
-    // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
+    // This class is used by the application to send email for account confirmation and password
+    // reset. For more details see https://go.microsoft.com/fwlink/?LinkID=532713
     public class EmailSender : IEmailSender
     {
         public Task SendEmailAsync(string email, string subject, string message)
         {
-            SmtpClient client = new SmtpClient("smtp.mailgun.org", 587);
-            client.UseDefaultCredentials = false;
-            //add credentials
-            client.Credentials = new NetworkCredential("postmaster@email.email.com", "xxxx");
-            client.EnableSsl = true;
+            SmtpClient client = new SmtpClient("smtp.mailgun.org", 587)
+            {
+                UseDefaultCredentials = false,
 
-            //MailMessage mailMessage = new MailMessage
-            //{
-            //    From = new MailAddress("email@email.com"),
-            //    Body = message,
-            //    IsBodyHtml = true
-            //};
-            MailMessage mailMessage = new MailMessage();
-            //from address
-            mailMessage.From = new MailAddress("email@email.com");
+                //add credentials
+                Credentials = new NetworkCredential("postmaster@email.email.com", "xxxx"),
+                EnableSsl = true
+            };
+
+            MailMessage mailMessage = new MailMessage
+            {
+                //from address
+                From = new MailAddress("email@email.com"),
+                Body = message,
+                IsBodyHtml = true,
+                Subject = subject
+            };
             mailMessage.To.Add(email);
-            mailMessage.Body = message;
-            mailMessage.IsBodyHtml = true;
-            mailMessage.Subject = subject;
 
-            //mailMessage.Body = message;
-            //mailMessage.IsBodyHtml = true;
             client.Send(mailMessage);
 
             return Task.CompletedTask;
